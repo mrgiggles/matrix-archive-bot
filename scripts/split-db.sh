e7a53086589d666a422946bf2fb3cbfecb7d144b
Steve Fink <sphink@gmail.com>
2025-06-20 17:42:31 -0700

    Add .mp4 extension to db files in hopes of defeating github range behavior w/compression. NOTE: This also requires hacking the webpacked sql.js output to append the .mp4

--- scripts/split-db.sh
+++ scripts/split-db.sh
@@ -12,9 +12,10 @@ bytes="$(stat --printf="%s" "$indb")"
 # smaller chunks = smaller git diffs
 serverChunkSize=$((1024 * 1024))
 suffixLength=3
+additionalSuffix=.mp4
 mkdir -p "$outdir"
 rm -f "$outdir/db.sqlite3"*
-split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes
+split "$indb" --bytes=$serverChunkSize "$outdir/db.sqlite3." --suffix-length=$suffixLength --numeric-suffixes --additional-suffix=$additionalSuffix
 
 # set request chunk size to match page size
 requestChunkSize="$(sqlite3 "$indb" 'pragma page_size')"
@@ -31,6 +32,7 @@ echo '
     "serverChunkSize": '$serverChunkSize',
     "urlPrefix": "db.sqlite3.",
     "suffixLength": '$suffixLength',
+    "additionalSuffix": "'$additionalSuffix'",
     "cacheBust": "'$cacheBust'"
 }
 ' > "$outdir/config.json"
